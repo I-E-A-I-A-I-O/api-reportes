96012d71f036cb587c41ddd0b7631311d04c1a28
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2021-08-10 11:56:49 -0400

    dbf endpoints

--- dev/index.ts
+++ dev/index.ts
@@ -3,6 +3,7 @@ dotenv.config()
 
 import Express from "express"
 import {router as mysql_report_router} from "./routers/reports/mysql/generate_reports"
+import {router as dbf_report_router} from "./routers/reports/dbf/generate_reports"
 
 const app = Express()
 const port = process.env.PORT || 8000
@@ -10,6 +11,7 @@ const port = process.env.PORT || 8000
 app.use(Express.json())
 
 app.use("/mysql", mysql_report_router)
+app.use("/dbf", dbf_report_router)
 
 app.listen(port, () => {
   console.info(`Servidor activo en el puerto ${port}.`)
